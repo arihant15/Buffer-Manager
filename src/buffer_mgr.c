ea068c1fa5815ebde2040226d8faeb3da245ad06
SyedSufyan <ssher@hawk.iit.edu>
2015-03-18 16:42:23 -0500

    added basic buffer and pool to work on

--- src/buffer_mgr.c
+++ src/buffer_mgr.c
@@ -5,6 +5,51 @@
 #include "dberror.h"
 #include "stdio.h"
 
+//we are using something similar to linked list to define the buffer struct
+
+// defining the buffer
+struct buffer {
+int pos;//positon of the page frame
+int pageNum;//page number
+bool dirty;//dirty page or not ?
+char *data;//data of the page
+int count;//general counter to count how old of the page
+struct buffer *next; // pointer to the next buffer
+} start=NULL;
+
+//function to calculate and return the length of the buffer pool
+int lenghtofPool()
+{
+int count = 0;
+struct buffer *root;
+root=first;//make root equals the first element in the buffer pool
+	while(root!=NULL)
+	{
+	count++;
+	root=root->next;//next
+	}
+return count;
+}
+
+//function returns the page frame number 
+int search(pageNumber pNum)
+{
+int framepos = -1
+struct buffer *temp;
+temp = start;
+	while (temp != NULL)
+	{
+	if(temp->pageNum = pNum)//if the page is found
+	{
+	framepos = temp->pos;//store it
+	break;
+	}
+	temp = temp->next;
+}
+return framepos;// return
+}
+
+
 char *testName;
 int main()
 {
@@ -56,7 +101,10 @@ RC forceFlushPool(BM_BufferPool *const bm)
 }
 
 // Buffer Manager Interface Access Pages
-RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page);
+RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page)
+{
+
+}
 RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page);
 RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page);
 RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
