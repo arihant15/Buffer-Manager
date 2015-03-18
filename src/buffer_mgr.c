e22e1f8c90690d69d409db1b184de91c9ff72ec0
arihant15 <arihantraj@gmail.com>
2015-03-17 22:31:27 -0500

    src
    src

--- src/buffer_mgr.c
+++ src/buffer_mgr.c
@@ -1,13 +1,48 @@
-#include "storage_mgr.h"
+#include "storage_mgr.c"
 #include "buffer_mgr.h"
 #include "buffer_mgr_stat.h"
 #include "dt.h"
+#include "dberror.h"
+#include "stdio.h"
 
+char *testName;
+int main()
+{
+	int a,i;
+	BM_BufferPool *bm = MAKE_POOL();
+  	testName = "Creating and Reading Back Dummy Pages";
+  	a = createPageFile("testbuffer.bin");
+  	BM_PageHandle *h = MAKE_PAGE_HANDLE();
+  	
+
+  	a = initBufferPool(bm, "testbuffer.bin", 3, RS_FIFO, NULL);
+  	printf("RC: %d Buffer Pool Initialization \n",a);
+}
 
 // Buffer Manager Interface Pool Handling
 RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, const int numPages, ReplacementStrategy strategy, void *stratData)
 {
+	if(bm->mgmtData != NULL)
+		return RC_BUFFER_POOL_ALREADY_INIT;
 
+	SM_FileHandle *fh;
+	fh=(SM_FileHandle *)malloc(sizeof(SM_FileHandle));
+
+    if(fh == NULL)
+    {
+        return RC_FILE_HANDLE_NOT_INIT;
+    }
+
+    int ret = openPageFile(pageFileName,fh);
+    if(ret == RC_FILE_NOT_FOUND)
+		return RC_FILE_NOT_FOUND;
+
+	bm->pageFile = pageFileName;
+    bm->numPages = numPages;
+    bm->strategy = strategy;
+    bm->mgmtData = fh;
+
+    return RC_OK;
 }
 
 RC shutdownBufferPool(BM_BufferPool *const bm)
@@ -17,7 +52,7 @@ RC shutdownBufferPool(BM_BufferPool *const bm)
 
 RC forceFlushPool(BM_BufferPool *const bm)
 {
-	
+
 }
 
 // Buffer Manager Interface Access Pages
